1728717fb7a68117923da9b698c9f9151369fb85
Dale Bewley <dbewley@redhat.com>
2025-06-09 20:17:28 -0700

    update deps for reference

--- layering/machineconfigs/.deps/99-worker-automount-sssd.d
+++ layering/machineconfigs/.deps/99-worker-automount-sssd.d
@@ -1,1 +1,1 @@
-99-worker-automount-sssd.yaml: 99-worker-automount-sssd.bu ../scripts/sssd.conf ../scripts/homedir.conf
+99-worker-automount-sssd.yaml: butane/99-worker-automount-sssd.bu ../scripts/sssd.conf ../scripts/homedir.conf
